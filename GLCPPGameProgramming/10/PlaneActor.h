8e30df99ee85e6e2990ca46cd0e13b078c55f8f7
Takuhiro Nishida <nishidatakuhiro1219@gmail.com>
2024-03-09 10:16:47 +0900

    Implemented 3D graphics for collision detection system (copied from 09)

---
+++ GLCPPGameProgramming/10/PlaneActor.h
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
+#include "Actor.h"
+
+class PlaneActor : public Actor {
+public:
+	PlaneActor(class Game* game);
+};
