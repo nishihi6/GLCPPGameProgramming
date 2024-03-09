350efa00f5700d509b235e27160da25762e67917
Takuhiro Nishida <nishidatakuhiro1219@gmail.com>
2024-03-10 03:52:58 +0900

    Implemented UI system

--- GLCPPGameProgramming/11/Font.h
+++ GLCPPGameProgramming/11/Font.h
@@ -6,25 +6,27 @@
 // See LICENSE in root directory for full details.
 // ----------------------------------------------------------------
 
+#pragma once
 #include <string>
+#include <unordered_map>
+#include <SDL/SDL_ttf.h>
+#include "Math.h"
 
-class Texture {
+class Font {
 public:
-	Texture();
-	~Texture();
+	Font(class Game* game);
+	~Font();
 
+	// Load/unload from a file
 	bool Load(const std::string& fileName);
 	void Unload();
-	void CreateFromSurface(struct SDL_Surface* surface);
 
-	void SetActive();
-
-	int GetWidth() const { return mWidth; }
-	int GetHeight() const { return mHeight; }
+	// Given string and this font, draw to a texture
+	class Texture* RenderText(const std::string& textKey,
+		const Vector3& color = Color::White,
+		int pointSize = 30);
 private:
-	// OpenGL ID of this texture
-	unsigned int mTextureID;
-	// Width/height of the texture
-	int mWidth;
-	int mHeight;
+	// Map of point sizes to font data
+	std::unordered_map<int, TTF_Font*> mFontData;
+	class Game* mGame;
 };
